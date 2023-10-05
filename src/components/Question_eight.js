0c0390eebd7d99011ee4dffe8ceaacc3e46df96b
(wanttobodybuilderifbbpro) <(wogud114@gmail.com)>
2023-10-05 17:31:42 +0900

    something has been done

---
+++ src/components/Question_eight.js
@@ -0,0 +1,15 @@
+function QuestionEight() {
+  // 이곳에 기능적인 부분들이 있으면 넣어준다
+
+  //이곳에서는 화면에 표시할 부분을 JSX 문법을 이용하여 넣어준다
+  return (
+    <div>
+      <p>8. 철학적 메시지를 담고 있는 소설이 좋다.</p>
+      <p>a. 매우 그렇다.</p>
+      <p>b. 상관없다.</p>
+      <p>c. 철학적 메시지가 없는 편이 좋다.</p>
+    </div>
+  );
+}
+
+export default QuestionEight;
---
+++ src/components/Question_nine.js
@@ -0,0 +1,16 @@
+function QuestionNine() {
+  // 이곳에 기능적인 부분들이 있으면 넣어준다
+
+  //이곳에서는 화면에 표시할 부분을 JSX 문법을 이용하여 넣어준다
+  return (
+    <div>
+      <p>9. 스케일이 큰 소설이 좋다.</p>
+      <p>a. 매우 그렇다.</p>
+      <p>b. 그렇다.</p>
+      <p>c. 상관없다.</p>
+      <p>d. 아니다.</p>
+    </div>
+  );
+}
+
+export default QuestionNine;
