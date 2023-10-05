402391f86c1a6ae3da488a62a7796076896ffaf2
(wanttobodybuilderifbbpro) <(wogud114@gmail.com)>
2023-10-05 23:22:36 +0900

    It is not working with another function.

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,18 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
 import "./App.css";
 import React, { Fragment } from "react";
+import QuestionOne from "../../components/Question_one";
 
 function App() {
   return (
     <>
-      <p>안녕하세요, 노블픽입니다.</p>
+      <div>
+        <p>안녕하세요, 노블픽입니다.</p>
 
-      <p>
-        질문에 응답하고 답을 얻으시려면 <Link>이곳</Link>을 클릭하세요
-      </p>
-      <button>
-        <span>Get Started</span>
-      </button>
+        <p>질문에 응답하고 답을 얻으시려면 이곳을 클릭하세요</p>
+        <button>
+          <Link to="/questionOne">
+            <span>Get Started</span>
+          </Link>
+        </button>
+        <Routes>
+          <Route path="/questionOne" element={<QuestionOne />} />
+        </Routes>
+      </div>
     </>
   );
 }
