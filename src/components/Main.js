7a9485e5c6c06e011ce1b4f376170560fabdae69
(wanttobodybuilderifbbpro) <(wogud114@gmail.com)>
2023-10-06 16:05:19 +0900

    subtly changed

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ function Main() {
       <p>안녕하세요, 노블픽입니다.</p>
 
       <p>
-        질문에 응답하고 답을 얻으시려면{" "}
+        질문에 응답하고 답을 얻으시려면
         <span className="bold-text">아래 Get Started 버튼</span>을 클릭하세요
       </p>
       <button>
