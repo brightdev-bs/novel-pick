580976506f32449ae7a243f659d9cd5156e97153
(wanttobodybuilderifbbpro) <(wogud114@gmail.com)>
2023-10-08 15:36:07 +0900

    chaged something

--- src/components/Question_one.js
+++ src/components/Question_one.js
@@ -6,11 +6,71 @@ function QuestionOne() {
   //이곳에서는 화면에 표시할 부분을 JSX 문법을 이용하여 넣어준다
   return (
     <div>
-      <p>1. 나는 웹소설의 전개가 빠를수록 좋다.</p>
-      <p>a. 매우 그렇다.</p>
-      <p>b. 그렇다.</p>
-      <p>c. 보통이다.</p>
-      <p>d. 아니다.</p>
+      <button
+        style={{
+          width: "300px",
+          minHeight: "80px",
+          fontSize: "15pt",
+          marginTop: "20px",
+          background: "#34AB64",
+          border: "0px",
+        }}
+      >
+        나는 웹소설의 전개가 빠를수록 좋다.
+      </button>
+      <br></br>
+      <button
+        style={{
+          width: "300px",
+          minHeight: "80px",
+          fontSize: "15pt",
+          marginTop: "20px",
+          background: "#34AB64",
+          border: "0px",
+        }}
+      >
+        a. 매우 그렇다.
+      </button>
+      <br></br>
+      <button
+        style={{
+          width: "300px",
+          minHeight: "80px",
+          fontSize: "15pt",
+          marginTop: "20px",
+          background: "#34AB64",
+          border: "0px",
+        }}
+      >
+        b. 그렇다.
+      </button>
+      <br></br>
+      <button
+        style={{
+          width: "300px",
+          minHeight: "80px",
+          fontSize: "15pt",
+          marginTop: "20px",
+          background: "#34AB64",
+          border: "0px",
+        }}
+      >
+        c. 보통이다.
+      </button>
+      <br></br>
+      <button
+        style={{
+          width: "300px",
+          minHeight: "80px",
+          fontSize: "15pt",
+          marginTop: "20px",
+          background: "#34AB64",
+          border: "0px",
+        }}
+      >
+        d. 아니다.
+      </button>
+      <br></br>
       <button>
         <Link to="/">처음으로</Link>
       </button>
