ae848961d9e9e536e39b59717de1260a4caf80a7
(wanttobodybuilderifbbpro) <(wogud114@gmail.com)>
2023-10-12 18:16:04 +0900

    kakaoSharButton

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState, Fragment } from "react";
 import Main from "../../components/Main";
 import QuestionOne from "../../components/Question_one";
